714d4aa619c0069652f1ec2c5117edfd4737ed30
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-12-06 10:51:18 -0300

    serving html, jpg, png, json and js with a simple switch

--- index.js
+++ index.js
@@ -47,16 +47,22 @@ const server = http.createServer((req, res) => {
       break;
   }
 
+  console.log(filePath, extname, contentType);
+
   fs.readFile(filePath, (err, data) => {
     if(err){
       if(err.code === 'ENOENT') {
         fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) =>{
-          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.writeHead(400, {'Content-Type': 'text/html'});
           res.end(data, 'utf-8');
         });
       } else {
-        
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
       }
+    } else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(data, 'utf-8');
     }
   });
 });
