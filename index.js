63a2da23d72eceecf085b0fa58cc4c4532667995
Tiago Diniz Gomes <tidgomes@gmail.com>
2021-12-06 10:40:55 -0300

    handling error page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
+  /* if (req.url === "/") {
     fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
       if (err) throw err;
       res.writeHead(200, { "Content-Type": "text/html" });
@@ -18,7 +18,47 @@ const server = http.createServer((req, res) => {
     ];
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(users));
+  } */
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extname = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (extname) {
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
+
+  fs.readFile(filePath, (err, data) => {
+    if(err){
+      if(err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) =>{
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.end(data, 'utf-8');
+        });
+      } else {
+        
+      }
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
